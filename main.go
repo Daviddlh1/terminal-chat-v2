52500a55a6da156dd5c20ac0d8459c35b8410d10
David De la Hoz <Daviddlh1@bitbucket.org>
2023-08-28 07:38:36 -0500

    creatin the terminal chat

--- main.go
+++ main.go
@@ -1,5 +1,79 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+type Client chan<- string
+
+var (
+	incomingClients = make(chan Client)
+	leavingClients  = make(chan Client)
+	messages        = make(chan string)
+)
+
+var (
+	host = flag.String("h", "localhost", "host")
+	port = flag.Int("p", 3090, "port")
+)
+
+func MessageWrite(conn net.Conn, messages <-chan string) {
+	for message := range messages {
+		fmt.Fprintln(conn, message)
+	}
+}
+
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	message := make(chan string)
+	go MessageWrite(conn, message)
+	clientName := conn.RemoteAddr().String()
+	message <- fmt.Sprintf("Welcome to the server, your name is: %s", clientName)
+	messages <- fmt.Sprintf("New client is here %s\n", clientName)
+	incomingClients <- message
+
+	inputMessage := bufio.NewScanner(conn)
+	for inputMessage.Scan() {
+		messages <- fmt.Sprintf("%s:%s", clientName, inputMessage.Text())
+	}
+
+	leavingClients <- message
+	messages <- fmt.Sprintf("%s: Said goodbye!", clientName)
+}
+
+func Broadcast() {
+	clients := make(map[Client]bool)
+	for {
+		select {
+		case message := <-messages:
+			for client := range clients {
+				client <- message
+			}
+		case newClient := <-incomingClients:
+			clients[newClient] = true
+		case leavingClient := <-leavingClients:
+			delete(clients, leavingClient)
+			close(leavingClient)
+		}
+	}
+}
+
 func main() {
-	
+	listener, err := net.Listen("tcp", "localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go Broadcast()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go HandleConnection(conn)
+	}
 }
